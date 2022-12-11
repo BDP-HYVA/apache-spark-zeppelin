4701c98a54a2e5e0a2388dcdf1493969f85bf5f0
HyeonJung Kim <90757714+hyeonjooong@users.noreply.github.com>
2022-12-11 19:30:26 +0900

    Update zeppelin.py
    sort 기능 추가

--- zeppelin.py
+++ zeppelin.py
@@ -1,13 +1,15 @@
 %spark2.pyspark
+
 from pyspark.sql.functions import *
 from pyspark.sql.functions import date_format
 
-
-textfile = sc.textFile("hdfs:///user/maria_dev/final/preprocessed02.csv")
-counts = textfile.flatMap(lambda line: line.split("\n"))\
+textfile = sc.textFile("hdfs:///user/maria_dev/final/my_real_love.csv")
+counts = textfile.flatMap(lambda line: line.split(","))\
 .map(lambda line: (line.split(" ")[0],1))\
-.reduceByKey(lambda a, b: a+b)
+.reduceByKey(lambda a, b: a+b)\
+.sortBy(lambda x: x[1], ascending=False)
 
 
 counts2 = counts.toDF()
-counts2.show()
+
+z.show(counts2)
